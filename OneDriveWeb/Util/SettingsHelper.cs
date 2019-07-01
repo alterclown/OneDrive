51c73724bc7d707bcb6ad2f18bcc96f63cf36290
alterclown <rahi2721@gmail.com>
2019-07-01 13:18:31 +0600

    changes

---
+++ OneDriveWeb/Util/SettingsHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace OneDriveWeb.Util
+{
+    public class SettingsHelper
+    {
+        public static string ClientId
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ida:ClientId"];
+            }
+        }
+        public static string ClientSecret
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ida:ClientSecret"];
+            }
+        }
+        public static string AzureAdInstance
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ida:AzureAdInstance"];
+            }
+        }
+        public static string AzureAdTenantId
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ida:AzureAdTenantId"];
+            }
+        }
+        public static string GraphResourceUrl
+        {
+            get { return "https://graph.microsoft.com/"; }
+        }
+        public static string AzureAdGraphResourceURL
+        {
+            get { return "https://graph.microsoft.com/"; }
+        }
+        public static string AzureAdAuthority
+        {
+            get
+            {
+                return AzureAdInstance + AzureAdTenantId;
+            }
+        }
+        public static string ClaimTypeObjectIdentifier
+        {
+            get { return "http://schemas.microsoft.com/identity/claims/objectidentifier"; }
+        }
+    }
+}
---
+++ SettingsHelper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Configuration;
+
+public class SettingsHelper
+{
+   
+
+}
