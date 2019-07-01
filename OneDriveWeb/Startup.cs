5184f77337dfb73a5a9ce804cd8ecb5dfccc0701
alterclown <rahi2721@gmail.com>
2019-07-01 09:20:10 +0600

    Add project files.

---
+++ OneDriveWeb/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace OneDriveWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
